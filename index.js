8164a972bb65b4bc2797ef7ab2bb19b309864ff9
maxbelgaid <max.belgaid@gmail.com>
2019-07-27 02:26:02 +0200

    Added a bit of security and the user_details update possibility - added order requests

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const morgan = require('morgan');
 
 const user = require('./routes/user');
 const category = require('./routes/category');
+const order = require('./routes/order');
 
 app.use(morgan('short'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(user);
 app.use(category);
+app.use(order);
 
 app.get('/', (req, res, next) => {
     console.log('THIS IS A / GET CALL');
